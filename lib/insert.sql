5c15e5dd5287318545bbef9493f99e4bedeb058d
Marty Warfel <marthawarfel@gmail.com>
2021-01-02 16:44:46 -0600

    commit tree 1636bfbc5676e0774750d1a93b42e67a1016145c

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+/* ID, NAME, AGE, SEX, COLOR, TEMPERAMENT, ALIVE */
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 5, "F", "brown", "kind", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 18, "F", "black", "funny", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 2, "F", "grizzly", "protective", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 7, "M", "brown", "loving", 1);
+INSERT INTO bears VALUES (5, "Melissa", 4, "F", "polar", "solitary", 1);
+INSERT INTO bears VALUES (6, "Grinch", 11, "F", "sun", "goofy", 1 );
+INSERT INTO bears VALUES (7, "Wendy", 6, "F", "sloth", "hungry", 1);
+INSERT INTO bears VALUES (8, NULL, 17, "M", "black", "offput", 0);
